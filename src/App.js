2000bd3c0d38596f7f25d3471902d821dab00e25
Joe Martinez <joedavid182@gmail.com>
2023-02-09 15:08:50 -0400

    primera revision

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ContactLisk from './components/contact_lisk';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ContactLisk></ContactLisk>
+
+    </div>
+  );
+}
+
+export default App;
+
